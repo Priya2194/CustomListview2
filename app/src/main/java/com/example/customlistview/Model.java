38112043e39e1a7d8a7193fcc486705596a9aa5f
Your Name <you@example.com>
2022-07-26 22:44:23 +0530

    CustomListView

---
+++ app/src/main/java/com/example/customlistview/Model.java
@@ -0,0 +1,41 @@
+package com.example.customlistview;
+
+public class Model
+{
+    String title;
+    String desc;
+    int icon;
+
+    public Model(String title, String desc, int icon) {
+        this.title = title;
+        this.desc = desc;
+        this.icon = icon;
+    }
+
+    public Model() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+}
